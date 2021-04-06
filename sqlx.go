4e8788ca1390947e64f15e6171d7ce2e943ed3cb
Your Name <you@example.com>
2021-04-06 23:36:08 +0800

    no message

--- sqlx.go
+++ sqlx.go
@@ -1,30 +1,47 @@
 package lhkhttp
 
 import (
-	"fmt"
-	_"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"sync"
-
 )
 var (
 	once sync.Once
+	sql *SqlSt
 )
 
 type SqlIF interface {
-
+    Close()
+    Query(str string)
 }
-func NewMysql()  SqlIF{
+type SqlSt struct {
+	database *sqlx.DB
+}
+func NewMysql(dns string)  SqlIF{
+
 	once.Do(func() {
+		if len(dns)==0 {
+			panic("mysql连接服务器的地址没有传")
+		}
+		//dns := "root:liuhangkai*#920@tcp(159.75.42.138:3306)/test1"
+		database, err := sqlx.Connect("mysql", dns)
+		if err!=nil {
+			panic("mysql服务器无法连接")
+		}
 
-		fmt.Print("=========once")
+		sql= new(SqlSt)
+		sql.database=database
+
 	})
-	fmt.Println("=======")
-   return nil
-}
-func InitMySql() (*sqlx.DB,error) {
-	dns := "root:liuhangkai*#920@tcp(159.75.42.138:3306)/test1"
-	database, err := sqlx.Connect("mysql", dns)
-	return database,err
 
+   return sql
 }
+
+func (s *SqlSt)Close () {
+	if sql !=nil{
+		sql.Close()
+	}
+}
+
+
+
