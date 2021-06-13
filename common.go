5af2d2b81af2b30da321cfd1992a2d5ddd138020
Your Name <you@example.com>
2021-06-13 21:25:29 +0800

    no message

--- common.go
+++ common.go
@@ -1,6 +1,8 @@
 package lhktools
 
 import (
+	"encoding/base64"
+	"fmt"
 	"github.com/google/uuid"
 	"net/url"
 	"time"
@@ -62,6 +64,10 @@ func GetSnowflakeId() int64 {
 	return 0
 }
 
+/*
+作用是在URL传参时如果直接传中文可能会出问题（对中文参数支持部完善），所以先用 Server.UrlEncode("中文参数");编码
+到另外一个页面接受的时候在用Server.UrlDecode("编码后参数一般为 %+ 16进制数的形式");解码获取中文参数
+*/
 func URLencode(str string) string {
 	return url.QueryEscape(str)
 	//url.QueryUnescape("")
@@ -72,4 +78,82 @@ func URLencode(str string) string {
 func URLDecode(str string)(string ,error) {
 	return url.QueryUnescape(str)
 }
+
+func Base64Encry(str string)string  {
+	 data:= base64.StdEncoding.EncodeToString([]byte(str))
+	 return string(data)
+}
+func Base64Decry(str string)(string,error)  {
+	data,er1:= base64.StdEncoding.DecodeString(str)
+	return string(data),er1
+}
+
+//默认是秒  1 是毫秒
+func GetTimeStamp(flag ...int) int64{
+	if len(flag)>0{
+		k:=flag[0]
+		if k ==1 {
+			return time.Now().UnixNano()/1000000
+		}
+	}
+	return  time.Now().Unix()
+}
+
+func GetTimeStampWithStr(str string) (int64 ,time.Time) {
+	//"2018-07-11 15:07:51"
+	loc, _ := time.LoadLocation("Asia/Shanghai")        //设置时区
+	tt, _ := time.ParseInLocation("2006-1-02 15:04:05", str, loc) //2006-01-02 15:04:05是转换的格式如php的"Y-m-d H:i:s"
+	return tt.Unix(),tt
+
+}
+
+func GetTimeStrWithTimeStamp(stamp int64,formateStr ...string) (string,time.Time){
+	fs:="2006-01-02 15:04:05"
+	if len(formateStr)>0 {
+		fs=formateStr[0]
+	}
+	fmt.Print(fs)
+	tm := time.Unix(stamp, 0)
+
+    return tm.Format(fs),tm
+}
+
+// getYearMonthToDay 查询指定年份指定月份有多少天
+// @params year int 指定年份
+// @params month int 指定月份
+func GetYearMonthToDay(year int, month int) int {
+	// 有31天的月份
+	day31 := map[int]bool{
+		1:  true,
+		3:  true,
+		5:  true,
+		7:  true,
+		8:  true,
+		10: true,
+		12: true,
+	}
+	if day31[month] == true {
+		return 31
+	}
+	// 有30天的月份
+	day30 := map[int]bool{
+		4:  true,
+		6:  true,
+		9:  true,
+		11: true,
+	}
+	if day30[month] == true {
+		return 30
+	}
+	// 计算是平年还是闰年
+	if (year%4 == 0 && year%100 != 0) || year%400 == 0 {
+		// 得出2月的天数
+		return 29
+	}
+	// 得出2月的天数
+	return 28
+}
+
+
+
 
--- common_test.go
+++ common_test.go
@@ -8,7 +8,27 @@ import (
 func TestGetUUID(t *testing.T) {
 	fmt.Println(GetUUID())
 	fmt.Println(GetSnowflakeId())
-	urlencodeStr:=URLencode("aaafdf=abcdefehttp://cdsdf")
+	urlencodeStr:=URLencode("dsafsadfunck尼米的没")
 	fmt.Println(urlencodeStr)
 	fmt.Println(URLDecode(urlencodeStr))
+
+    base64encry:=Base64Encry("刘航凯信息")
+	decry, err := Base64Decry(base64encry)
+	fmt.Println(base64encry,decry,err)
+
+}
+
+func TestArivie(t *testing.T)  {
+	fmt.Println(GetTimeStamp())
+	fmt.Println(GetTimeStamp(0))
+	fmt.Println(GetTimeStamp(1))
+	fmt.Println(GetTimeStampWithStr("2021-06-13 18:27:31"))
+	fmt.Println(GetTimeStrWithTimeStamp(1623580052))
+    fmt.Println(GetYearMonthToDay(2021,2))
+
+	_, time1 := GetTimeStampWithStr("1987-01-23 00:00:00")
+	_, time2 := GetTimeStampWithStr("1987-01-24 00:00:00")
+	fmt.Println(time2.Sub(time1))
+
+
 }
